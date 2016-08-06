ed592f045152856f48634bda43b8ee1e6b60bfee
Chris Kastorff <encryptio@gmail.com>
2016-08-06 10:04:51 -0700

    rename files

---
+++ event.go
@@ -0,0 +1,17 @@
+package evsrc
+
+// Events are sent by ServerConns and received by ClientConns.
+//
+// Note that the ID field is the id sent in this specific Event, and does not
+// correspond to the HTML5 lastEventId field for client connections, which is
+// instead a field on ClientConn.
+type Event struct {
+	Event string
+	Data  []byte
+	ID    string
+	Retry int
+}
+
+func (e Event) IsZero() bool {
+	return e.Event == "" && e.Data == nil && e.ID == "" && e.Retry == 0
+}
